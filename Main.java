32c229f6f34b586c37e42b5474522b17b5723cbd
Emanuel-cpu-dev <68510301+Emanuel-cpu-dev@users.noreply.github.com>
2023-06-29 15:20:19 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+package Main;
+
+import Views.LoginView;
+
+
+public class Main {
+    public static void main(String[] args) { 
+        
+        //Instancia del login
+        LoginView login = new LoginView();
+        login.setVisible(true);
+         
+    }
+    
+}
